6183c39f36f11d16fd504b741bb8e9e6fbbde35e
vatsshubham87 <vatsshubham87@gmail.com>
2024-10-24 15:42:57 +0530

    implemented config server

---
+++ src/test/java/com/cofig/server/Config/Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofig.server.Config.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
